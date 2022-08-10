fe3036df65da00d4e8d1ce05d86f2a8bca1a861a
Arafat <yeamin@sheba.xyz>
2022-08-10 22:25:18 +0600

    [WIP] #movie detaills page

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,6 +1,6 @@
- import { useState, useEffect } from 'react'; 
- import axios from '../../axios';
- import "./Row.css";
+import {useState, useEffect} from 'react';
+import axios from '../../axios';
+import "./Row.css";
 import CardWrapper from "../Movies/CardWrapper";
 import CardImage from "..//Movies/CardImage";
 import CardTitle from "../Movies/CardTitle";
@@ -8,105 +8,85 @@ import CardDescription from "../Movies/CardDescription";
 import CardFeatureWrapper from "../Movies/CardFeatureWrapper";
 import CardFeatureClose from "../Movies/CardFeatureClose";
 import PlayerVideo from "../Movies/PlayerVideo";
- import PlayButton from "../Header/PlayButton";
- import PlayerOverlay from "../Movies/PlayerOverlay";
- import AllCardsWrapper from "../Movies/AllCardsWrapper";
+import PlayButton from "../Header/PlayButton";
+import PlayerOverlay from "../Movies/PlayerOverlay";
+import AllCardsWrapper from "../Movies/AllCardsWrapper";
 
 
+import React from 'react';
 
- import React from 'react';
+const base_url = "https://image.tmdb.org/t/p/original/";
 
- const base_url = "https://image.tmdb.org/t/p/original/";
-
- function Row({title, fetchUrl, isLargeRow }) {
-     const [movies, setMovies] = useState([]);
-     const [trailerUrl, setTrailerUrl] = useState("");
+function Row({title, fetchUrl, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
     const [showCardFeature, setShowCardFeature] = useState(false);
-     const [activeItem, setActiveItem] = useState(false);
-     const [showPlayer, setShowPlayer] = useState(false);
+    const [activeItem, setActiveItem] = useState(false);
+    const [showPlayer, setShowPlayer] = useState(false);
 
-    
-     useEffect(() => {
+
+    useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
-        
+
             return request;
         }
+
         fetchData();
-     }, [fetchUrl]);
+    }, [fetchUrl]);
 
-     const opts = {
-         height: "390",
-         width: "100%",
-         playerVars: {
-             autoplay:1,
-         },
-     };
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
 
-     const handleClick = (movie) => {
-         setShowCardFeature(true);
-         setActiveItem(movie);
-         console.log(activeItem);
-     }
-    
-     return (
-         <div className="row">  
-             <h2>{title}</h2>
-             <AllCardsWrapper>
-             <div className = "row-posters">
-                 {movies.map(movie => (
-                    <img
-                        onClick={() => handleClick(movie)}
-                        className={`row-poster ${isLargeRow && "row-posterLarge"}`}
-                        key={movie.id} 
-                        src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                        alt={movie.name} 
-                    />
-                 ))}
-             </div>
-             </AllCardsWrapper>
-            {/* (showCardFeature) ? {*/}
-            {/*    <CardFeatureWrapper*/}
-            {/*    style={{*/}
-            {/*      backgroundImage: `url($${base_url}${ isLargeRow ? activeItem.poster_path : activeItem.backdrop_path})`,*/}
-            {/*    }}*/}
-            {/*  >*/}
-            {/*    <CardTitle>{activeItem.name}</CardTitle>*/}
-            {/*    <CardDescription>{activeItem.description}</CardDescription>*/}
-            {/*    <CardFeatureClose onClick={() => setShowCardFeature(false)} />*/}
-            {/*    <PlayButton onClick={() => setShowPlayer(true)}>*/}
-            {/*      Play*/}
-            {/*    </PlayButton>*/}
-            {/*    {showPlayer ? (*/}
-            {/*      <PlayerOverlay onClick={() => setShowPlayer(false)}>*/}
-            {/*        <PlayerVideo src="../videos/video.mp4" type="video/mp4" />*/}
-            {/*      </PlayerOverlay>*/}
-            {/*    ) : null}*/}
-            {/*  </CardFeatureWrapper>*/}
-            {/*} : null*/}
-             {showCardFeature ? (
-                 <CardFeatureWrapper
-                     style={{
-                       backgroundImage: `url(${base_url}${ activeItem.backdrop_path })`,
+    const handleClick = (movie) => {
+        setShowCardFeature(true);
+        setActiveItem(movie);
+    }
 
-                     }}
-                 >
-                     <CardTitle>{activeItem.name ? activeItem.name : activeItem.title}</CardTitle>
-                     <CardDescription>{activeItem.overview}</CardDescription>
-                     <CardFeatureClose onClick={() => setShowCardFeature(false)} />
-                     <PlayButton onClick={() => setShowPlayer(true)}>
-                         Play
-                     </PlayButton>
-                     {showPlayer ? (
-                         <PlayerOverlay onClick={() => setShowPlayer(false)}>
-                             <PlayerVideo src="../videos/video.mp4" type="video/mp4" />
-                         </PlayerOverlay>
-                     ) : null}
-                 </CardFeatureWrapper>
-             ) : null}
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <AllCardsWrapper>
+                <div className="row-posters">
+                    {movies.map(movie => (
+                        <img
+                            onClick={() => handleClick(movie)}
+                            className={`row-poster ${isLargeRow && "row-posterLarge"}`}
+                            key={movie.id}
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            alt={movie.name}
+                        />
+                    ))}
+                </div>
+            </AllCardsWrapper>
+            {showCardFeature ? (
+                <CardFeatureWrapper
+                    style={{
+                        backgroundImage: `url(${base_url}${activeItem.backdrop_path})`,
+
+                    }}
+                >
+                    <CardTitle>{activeItem.name ? activeItem.name : activeItem.title}</CardTitle>
+                    <CardDescription>{activeItem.overview}</CardDescription>
+                    <CardFeatureClose onClick={() => setShowCardFeature(false)}/>
+                    <PlayButton onClick={() => setShowPlayer(true)}>
+                        Play
+                    </PlayButton>
+                    {showPlayer ? (
+                        <PlayerOverlay onClick={() => setShowPlayer(false)}>
+                            <PlayerVideo src="../videos/video.mp4" type="video/mp4"/>
+                        </PlayerOverlay>
+                    ) : null}
+                </CardFeatureWrapper>
+            ) : null}
         </div>
     )
- }
+}
 
- export default Row 
+export default Row
