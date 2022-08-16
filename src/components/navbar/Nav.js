5045354c8e8a16b9de1550c5670801430c3041f7
Arafat <yeamin@sheba.xyz>
2022-08-16 23:10:16 +0600

    [ADD] #Navbar design complete

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -8,7 +8,7 @@ import Logo from "./Logo";
 
 function Nav({dynamicClass}) {
     const [show, handleShow] = useState(false);
-    const [signedIn, handleSignin] = useState(false);
+    const [signedIn, handleSignin] = useState(true);
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -22,7 +22,7 @@ function Nav({dynamicClass}) {
         };
     }, [])
     return (
-        <div className={`navbar`}>
+        <div className={`navbar d-flex space-between flex-center flex-middle`}>
             <div className={`container nav  ${dynamicClass} ${show && "nav-black"}`}>
                 {/*<div className=>*/}
                 <div className="left">
@@ -40,17 +40,65 @@ function Nav({dynamicClass}) {
                 <div className="right">
                     {signedIn ?
                         <div>
-                            <SearchIcon className={"icon"}/>
-                            <span>Kids</span>
-                            <Notifications className={"icon"}/>
-                            <img src={`${process.env.PUBLIC_URL}/default_profile.jpeg`} alt=""/>
-                            <div className="profile">
-                                <ArrowDropDown className={"icon"}/>
-                                <div className="options">
-                                    <span>Watch List</span>
-                                    <span>Settings</span>
-                                    <span>Logout</span>
+                            <div className="righticons d-flex flex-end flex-middle">
+                                <a href={"#"}> <SearchIcon fontSize={"large"} className={"icon"}/> </a>
+                                <div className="dropdown notification">
+                                    <img src="/images/icons/notification.svg" alt="notificatio icon"/>
+                                    <div className="dropdown-content">
+                                        <a href="#" className="profile-item d-flex flex-middle">
+                                            <img src="/images/icons/user2.png" alt="user profile icon"
+                                                 className="user-icon"/>
+                                            <span>You have new notification from <span>User 123</span></span>
+                                        </a>
+                                        <a href="#" className="profile-item d-flex flex-middle">
+                                            <img src="/images/icons/user1.png" alt="user profile icon"
+                                                 className="user-icon"/>
+                                            <span>You have new notification from <span>User 123</span></span>
+                                        </a>
+                                        <a href="#" className="profile-item d-flex flex-middle">
+                                            <img src="/images/icons/user4.png" alt="user profile icon"
+                                                 className="user-icon"/>
+                                            <span>You have new notification from <span>User 123</span></span>
+                                        </a>
+                                        <a href="#" className="profile-item d-flex flex-middle">
+                                            <img src="/images/icons/user3.png" alt="user profile icon"
+                                                 className="user-icon"/>
+                                            <span>You have new notification from <span>User 123</span></span>
+                                        </a>
+                                    </div>
                                 </div>
+
+                                <div className="dropdown">
+                                    <img src={`${process.env.PUBLIC_URL}/images/icons/user2.png`}  alt="user profile icon"
+                                         className="user-icon"/> <span className="profile-arrow"></span>
+
+                                    <div className="dropdown-content">
+                                        <div className="profile-links">
+                                            <a href="#" className="profile-item d-flex flex-middle">
+                                                <img src={`${process.env.PUBLIC_URL}/default_profile.jpeg`}  alt="user profile icon"
+                                                     className="user-icon"/>
+                                                <span>Rajesh</span>
+                                            </a>
+                                            <a href="#" className="profile-item d-flex flex-middle">
+                                                <img src="/images/icons/user2.png" alt="user profile icon"
+                                                     className="user-icon"/>
+                                                <span>Karan</span>
+                                            </a>
+                                            <a href="#" className="profile-item d-flex flex-middle">
+                                                <img src="/images/icons/user3.png" alt="user profile icon"
+                                                     className="user-icon"/>
+                                                <span>Pappy</span>
+                                            </a>
+                                            <a href="#" className="profile-item d-flex flex-middle">
+                                                <img src="/images/icons/user4.png" alt="user profile icon"
+                                                     className="user-icon"/>
+                                                <span>Denny</span>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                         :
