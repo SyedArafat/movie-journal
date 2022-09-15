81b819ed7e773aa766dccc259245184cb12889b8
Arafat <yeamin@sheba.xyz>
2022-09-16 00:52:12 +0600

    [UPDATE] #scrollbar option added with design

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect} from 'react';
 import "./Row.css";
 import AllCardsWrapper from "../Movies/AllCardsWrapper";
 import React from 'react';
@@ -9,6 +9,24 @@ import {DeleteToken} from "../../auth/Authentication";
 
 const base_url = `${IMAGE_BASE_URL}w500`;
 
+const sideScroll = (element, speed, distance, step) => {
+    let scrollAmount = 0;
+    const slideTimer = setInterval(() => {
+        // console.log(element.scrollWidth);
+        // console.log(element.clientWidth);
+        element.scrollLeft += step;
+        scrollAmount += Math.abs(step);
+        if (scrollAmount >= distance) {
+            clearInterval(slideTimer);
+        }
+    }, 18);
+};
+
+const showScroll = (element) => {
+    return (element?.scrollWidth > element?.clientWidth);
+}
+
+
 function Row({title, fetchUrl, isLargeRow, setLoading}) {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
@@ -17,13 +35,6 @@ function Row({title, fetchUrl, isLargeRow, setLoading}) {
     const [showCardFeature, setShowCardFeature] = useState(false);
     const [activeItem, setActiveItem] = useState(false);
     const [reload, setReload] = useState(false);
-
-    const ref = useRef();
-
-    const scroll = (scrollOffset) => {
-        console.log("asdasdasd");
-        ref.current.scrollLeft += scrollOffset;
-    };
 
 
     useEffect(() => {
@@ -51,50 +62,48 @@ function Row({title, fetchUrl, isLargeRow, setLoading}) {
 
         handleOpen();
     }
+    const contentWrapper = React.useRef(null);
 
-    return (
-        <div className="row">
-            {
-                movies.length !== 0 ?
-                    <>
-                        <div className="header">
-                            <h2>{title}</h2>
-                            <div className="progress-bar"></div>
-                        </div>
-                        <AllCardsWrapper>
-                            <button className="handle left-handle">
-                                <div className="text">&#8249;</div>
-                            </button>
-                            <div className="row-posters">
-                                {movies.map(movie => (
-                                    <div key={movie.id} className={`row-poster ${isLargeRow && "row-posterLarge"}`}>
-                                        <img
-                                            onClick={() => handleClick(movie)}
-                                            className={`row-poster ${isLargeRow && "row-posterLarge"}`}
-                                            key={movie.id}
-                                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                                            alt={movie.name}
-                                        />
-                                    </div>
-                                ))}
+    return (<div className="row">
+        {movies.length !== 0 ? <>
+            <div>
+                <h2>{title}</h2>
+            </div>
+            <AllCardsWrapper>
+                {showScroll(contentWrapper.current) && <button className="handle left-handle">
+                    <div
+                        onClick={() => {
+                            sideScroll(contentWrapper.current, 10, 300, -20);
+                        }}
+                        className="text">&#8249;</div>
+                </button>}
+                <div className="row-posters" ref={contentWrapper}>
+                    {movies.map(movie => (
+                        <div key={movie.id} className={`row-poster ${isLargeRow && "row-posterLarge"}`}>
+                            <img
+                                onClick={() => handleClick(movie)}
+                                className={`row-poster ${isLargeRow && "row-posterLarge"}`}
+                                key={movie.id}
+                                src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                                alt={movie.name}
+                            />
+                        </div>))}
 
-                            </div>
-                            <button className="handle right-handle">
-                                <div onClick={() => scroll(20)} className="text">&#8250;</div>
-                            </button>
-                        </AllCardsWrapper>
-                    </> : null
-            }
-            {
-                showCardFeature ?
-                    <ConditionalMovieModalWrapper setIsUpdated={setReload} modalOpen={open}
-                                                  onClose={() => setOpen(false)}
-                                                  activeItem={activeItem} isTV={isTV}/>
-                    : null
-            }
+                </div>
+                <button className="handle right-handle">
+                    {showScroll(contentWrapper.current) && <div
+                        onClick={() => {
+                            sideScroll(contentWrapper.current, 10, 300, 20);
+                        }}
+                        className="text">&#8250;</div>}
+                </button>
+            </AllCardsWrapper>
+        </> : null}
+        {showCardFeature ? <ConditionalMovieModalWrapper setIsUpdated={setReload} modalOpen={open}
+                                                         onClose={() => setOpen(false)}
+                                                         activeItem={activeItem} isTV={isTV}/> : null}
 
-        </div>
-    )
+    </div>)
 }
 
 export default Row
