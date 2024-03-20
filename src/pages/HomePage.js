59846137f3dcca859ea65ad008f4305c94813f90
Arafat <yeamin@sheba.xyz>
2024-03-20 17:21:34 +0600

    home page single api call

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,12 @@ import Banner from "../components/banner/Banner";
 import Row from "../components/row/Row";
 import requests from "../config/requests";
 import Footer from "../compounds/FooterCompound";
-import {BACKEND_EXTERNAL_SEARCH, BACKEND_HOME_API_AUTH} from "../config/config";
+import {BACKEND_EXTERNAL_SEARCH, BACKEND_HOME_API_AUTH, BACKEND_HOME_API_NON_AUTH} from "../config/config";
 import React, {useEffect, useState} from "react";
 import SearchResults from "../components/Search/SearchResults";
 import Loader from "../components/Loader";
-import {Authed, DeleteToken} from "../auth/Authentication";
-import {GetApi, HomeApiGet} from "../api/MediaContentClient";
+import {Authed} from "../auth/Authentication";
+import {GetApi} from "../api/MediaContentClient";
 
 function HomePage() {
     const [loading, setLoading] = useState(false);
@@ -17,29 +17,20 @@ function HomePage() {
     const [homeData, setHomeData] = useState([]);
 
     useEffect(() => {
+        const url = Authed() ? BACKEND_HOME_API_AUTH : BACKEND_HOME_API_NON_AUTH;
         async function fetchData() {
             try {
-                setLoading(true); // Set loading to true before making the API call
-                const response = await GetApi(BACKEND_HOME_API_AUTH);
-                // console.log(response.data.recentwatchedmovie)// Make the API call
-                setHomeData(response.data); // Set the movies state with the API response data
+                setLoading(true);
+                const response = await GetApi(url);
+                setHomeData(response.data);
             } catch (error) {
                 console.error("Error fetching data:", error);
             } finally {
-                setLoading(false); // Set loading to false after the API call completes (whether success or error)
+                setLoading(false);
             }
         }
 
-        fetchData().then(r => {
-            // console.log(homeData);
-        }); // Call the fetchData function when the component is mounted
-
-        // Cleanup function (optional) - executed when the component is unmounted
-        return () => {
-            // console.log(homeData);
-
-            // Any cleanup code can be placed here
-        };
+        fetchData().then(r => {});
     }, []);
 
 
@@ -62,6 +53,7 @@ function HomePage() {
         }
         setLoading(false);
     }
+
     return (
         <div className="App">
             <Nav callback={searchItems} setLoading={setLoading}/>
@@ -71,25 +63,20 @@ function HomePage() {
                     <Banner setLoading={setLoading} movie={homeData.banner}/>
 
                     {
-                        // console.log(Authed())
                         (!Authed()) ?
                             <>
-                                <Row setLoading={setLoading} isLargeRow={true} title="Trending Now"
-                                     fetchUrl={requests.fetchTrending}/>
-                                <Row setLoading={setLoading} isLargeRow={true} title="NETFLIX ORIGINALS"
-                                     fetchUrl={requests.fetchNetflixOriginals}/>
-                                <Row setLoading={setLoading} isLargeRow={true} title="Top Rated"
-                                     fetchUrl={requests.fetchTopRated}/>
+                                {homeData.trending && <Row setLoading={setLoading} isLargeRow={true} title="Trending Now"
+                                     fetchUrl={requests.fetchTrending} movies={homeData.trending}/>}
+                                {homeData.netflixoriginals && <Row setLoading={setLoading} isLargeRow={true} title="NETFLIX ORIGINALS"
+                                     fetchUrl={requests.fetchNetflixOriginals} movies={homeData.netflixoriginals}/>}
+                                {homeData.toprated && <Row setLoading={setLoading} isLargeRow={true} title="Top Rated"
+                                     fetchUrl={requests.fetchTopRated} movies={homeData.toprated}/>}
                             </> :
                             <>
-                                {/*<Row setLoading={setLoading} isLargeRow={true} title="Recent Watched"*/}
-                                {/*     fetchUrl={requests.recentWatched} movies={homeData.recentwatchedmovie}/>*/}
-
                                 {homeData.recentwatchedmovie && <Row setLoading={setLoading} isLargeRow={true} title="Watched Movies"
                                      fetchUrl={requests.recentWatchedMovies} movies={homeData.recentwatchedmovie}/>}
                                 {homeData.recentwatchedtv && <Row setLoading={setLoading} isLargeRow={true} title="Watched TV"
                                      fetchUrl={requests.recentWatchedTV} movies={homeData.recentwatchedtv}/>}
-
                                 {homeData.trending && <Row setLoading={setLoading} isLargeRow={true} title="Trending Now"
                                      fetchUrl={requests.fetchTrending} movies={homeData.trending}/>}
                             </>
